964cbc070b647ea71ed0c8e43f7c9f7147f0d76b
rainy-me <github@rainy.me>
2020-04-01 21:19:04 +0900

    fix: fix build

--- packages/script/webpack.server.ts
+++ packages/script/webpack.server.ts
@@ -6,13 +6,20 @@ const serverSrc = path.resolve(__dirname, "./server.ts");
 const distRoot = path.resolve(__dirname, "./dist");
 
 const serverConfig: Configuration = {
-  mode: "development",
+  mode: "production",
   target: "node",
   node: {
     __dirname: false,
     __filename: false
   },
-  externals: [nodeExternals()],
+  externals: [
+    (_, req, cb) => {
+      if (!/^@package/.test(req) && req[0] !== "." && req[0] !== "/") {
+        return cb(null, `commonjs ${req}`);
+      }
+      cb(undefined, undefined);
+    }
+  ],
   entry: {
     server: serverSrc
   },
@@ -20,9 +27,7 @@ const serverConfig: Configuration = {
     path: distRoot,
     filename: "server.js"
   },
-  resolve: {
-    extensions: [".js", ".ts"]
-  },
+  resolve: { symlinks: false, extensions: [".js", ".ts"] },
   plugins: [
     new webpack.DefinePlugin({
       "process.env": JSON.stringify(process.env)
